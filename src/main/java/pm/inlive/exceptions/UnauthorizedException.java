d18a5269dd7bd1cc35838db43bf8e530b0e81979
DAssanbayev <DAssanbayev@fortebank.com>
2026-03-29 16:18:10 +0500

    feat: add exceptions, handlers and validators

---
+++ src/main/java/pm/inlive/exceptions/UnauthorizedException.java
@@ -0,0 +1,11 @@
+package pm.inlive.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+
+    public UnauthorizedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
